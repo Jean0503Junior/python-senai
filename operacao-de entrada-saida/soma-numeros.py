e0d84f1b589a152003d715b187ad157ca39991a1
Jean0503Junior <jeanjr0503@gmail.com>
2026-09-17 14:02:24 -0300

    operadores lógicos e entrada de dados

---
+++ operacao-de entrada-saida/soma-numeros.py
@@ -0,0 +1,15 @@
+# Entrada de dados básicos
+n1 = int (input("Digite o primeiro número: "))
+n2 = int (input("Digite o segundo número: "))
+
+# Processamento computacional
+soma = n1 + n2
+subtracao = n1 - n2
+divisao = n1 / n2
+multi = n1 * n2
+
+# Sáida da informação
+print("O resultado da soma dos números é:", soma)
+print("O resultado da subtração dos números é:", subtracao)
+print("O resultado da divisão dos números é:", divisao)
+print("O resultadi da multiplicação dos números é:", multi)
