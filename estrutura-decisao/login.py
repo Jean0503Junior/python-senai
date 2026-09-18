9b198fa2fe5507d3ba0380e7123c027267d215a2
Jean0503Junior <jeanjr0503@gmail.com>
2026-09-18 14:31:06 -0300

    Adicionando exercício de verificação de login com if else

---
+++ estrutura-decisao/login.py
@@ -0,0 +1,11 @@
+# Solicita Login do usuário
+login = input("Digite seu login: ")
+
+# Solicita a senha do usuário
+senha = input("Digite sua senha: ")
+
+# Verifica se o login e a senha estão corretos
+if login == "admin" and senha == "1234":
+    print("Seja bem vindo!!")
+else:
+    print("Dados incorretos")
